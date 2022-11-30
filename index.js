94b746c71dbcae5b49c33c066505dc60f70c6d9a
tm-kimura <tm-kimura@advanced-media.co.jp>
2022-11-30 13:53:57 +0900

    listBucketsをコメントアウト

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ aws.config.update({region: 'ap-northeast-1'});
 
 const s3 = new aws.S3();
 
-s3.listBuckets((err, data) => {
+/* s3.listBuckets((err, data) => {
     if (err) {
         console.log(err, err.stack);
     }
     else if(data) {
         console.log(data.Buckets);
     }
-});
+}); */
 
 const bucketName = 'aoi-wiki-bucket';
 var file = './kimura_test.txt';
@@ -34,6 +34,7 @@ s3.upload(uploadParams, (err, data) => {
             console.log(err, err.stack);
     }
     else if(data){
+        console.log('upload success.');
         console.log(data.Location);
     }
 });
